9f9f2a6a0c03cfaf63cbbf4b4e4cfb8a5e0f6d2a
Stefan Boos <kontakt@boos.systems>
2023-03-27 07:24:26 +0200

    feat: add intel x64 version for markdown-linked-images-mover

--- Casks/markdown-linked-images-mover.rb
+++ Casks/markdown-linked-images-mover.rb
@@ -1,11 +1,19 @@
 cask "markdown-linked-images-mover" do
-  version "0.1.0-alpha"
-  sha256 "22593ffcb0a265e1f2488edf4c3adfc8d8edacd4913fd039a45e0530a40dd735"
+  version "0.1.1-alpha"
 
-  url "https://github.com/wonderbird/markdown-linked-images-mover/releases/download/v#{version}/markdown-linked-images-mover.zip"
+  if arch == ":intel" then
+    sha256 "6a117958dda1ecb5337852e84643bac1269b7358eff46c383bfd30f2340bdedc"
+    url "https://github.com/wonderbird/markdown-linked-images-mover/releases/download/v#{version}/markdown-linked-images-mover.x64.bottle.zip"
+  else
+    sha256 "b98cdfb7932d3d2f6be7ae1dbe91a0a845c52e88ce89a213d7ad78cabbc5f257"
+    url "https://github.com/wonderbird/markdown-linked-images-mover/releases/download/v#{version}/markdown-linked-images-mover.arm64.bottle.zip"
+  end
+
   name "markdown-linked-images-mover"
   desc "Move all images used by a markdown file into a folder"
   homepage "https://github.com/wonderbird/markdown-linked-images-mover"
 
-  binary "MarkdownLinkedImagesMover"
+  depends_on macos: ">= :monterey"
+
+  binary "MarkdownLinkedImagesMover", target: "malimo"
 end

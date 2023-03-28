8d325612dc154e7fe68a88bb2a3f473663fe6cb1
Stefan Boos <kontakt@boos.systems>
2023-03-28 05:35:54 +0200

    fix: use arm64 on arm/M1 and x64 on intel

--- Casks/markdown-linked-images-mover.rb
+++ Casks/markdown-linked-images-mover.rb
@@ -1,13 +1,12 @@
 cask "markdown-linked-images-mover" do
+  arch arm: "arm64", intel: "x64"
+  
   version "0.1.2-alpha"
 
-  if arch == ":arm64"
-    sha256 "fcdc70818944f0960c32d032c1e6e2688b395ea313bfaa04b1dccb1c47ecb97a"
-    url "https://github.com/wonderbird/markdown-linked-images-mover/releases/download/v#{version}/markdown-linked-images-mover.arm64.bottle.zip"
-  else
-    sha256 "be74b02135a6340687f2b096dc70bb06655995a8592add7822a978438da5beee"
-    url "https://github.com/wonderbird/markdown-linked-images-mover/releases/download/v#{version}/markdown-linked-images-mover.x64.bottle.zip"
-  end
+  sha256 arm:   "fcdc70818944f0960c32d032c1e6e2688b395ea313bfaa04b1dccb1c47ecb97a",
+         intel: "be74b02135a6340687f2b096dc70bb06655995a8592add7822a978438da5beee"
+
+  url "https://github.com/wonderbird/markdown-linked-images-mover/releases/download/v#{version}/markdown-linked-images-mover.#{arch}.bottle.zip"
 
   name "markdown-linked-images-mover"
   desc "Move all images used by a markdown file into a folder"

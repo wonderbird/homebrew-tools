4c4a4069efd1cc01590f7943943e598a1c7b7b33
Stefan Boos <kontakt@boos.systems>
2023-05-21 19:23:34 +0200

    feat: malimo v0.1.8-alpha
    see https://github.com/wonderbird/malimo/releases/tag/v0.1.8-alpha

--- Casks/malimo.rb
+++ Casks/malimo.rb
@@ -2,9 +2,9 @@ cask "malimo" do
   arch arm:   "osx.12-arm64",
        intel: "osx.12-x64"
 
-  version "0.1.7-alpha"
-  sha256 arm:   "710cd9a7f8cd3d3b3faa3ab269b73f1b9438581c6c2ebfc20af7440681c0c925",
-         intel: "2b5fd30bf31e6ab2d5b94334d85ecece63f9d5b540a6da7225b8d06989a75fbb"
+  version "0.1.8-alpha"
+  sha256 arm:   "f9303186f33c1410c860634128312b4a193505e0ba85a8ae048074b2c06fe7cc",
+         intel: "019ef1ad97245d2af775f853cc60143a3703c2c745e3aec45c62c814a69182b8"
 
   url "https://github.com/wonderbird/malimo/releases/download/v#{version}/malimo.#{arch}.bottle.zip"
   name "malimo"
